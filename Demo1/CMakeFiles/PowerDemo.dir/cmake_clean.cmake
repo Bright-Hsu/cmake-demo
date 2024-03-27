948df654d8f9c4bb6ff20a3b3d880c6788b5fa2c
xuliang <2191579187@qq.com>
2024-03-27 20:22:20 +0800

    update demo1

---
+++ Demo1/CMakeFiles/PowerDemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PowerDemo.dir/main.cc.o"
+  "PowerDemo"
+  "PowerDemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PowerDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
